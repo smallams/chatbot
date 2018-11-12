1bf35d005a323601ce02cfd15f16fd4d4a4c5bb1
smallams <ronlouiemarte@gmail.com>
2018-11-12 22:18:58 +0800

    test

--- config.js
+++ config.js
@@ -6,4 +6,4 @@ module.exports = {
     GOOGLE_PROJECT_ID: process.env.GOOGLE_PROJECT_ID,
     GOOGLE_CLIENT_EMAIL: process.env.GOOGLE_CLIENT_EMAIL,
     GOOGLE_PRIVATE_KEY: process.env.GOOGLE_PRIVATE_KEY
-};
+}; //test
